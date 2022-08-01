819f196778e64b2b05d9ee5dee51e196a5814889
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-08-01 13:40:48 +0300

    Initial commit

---
+++ src/main/java/com/daniel/alittlecompany/ALittleCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.alittlecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALittleCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ALittleCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/alittlecompany/ALittleCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.alittlecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ALittleCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
